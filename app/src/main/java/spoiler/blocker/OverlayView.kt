323df947bb6b16c7aa3bad75ace71f9d89845942
Muthuraj <muthu.sattur@gmail.com>
2022-03-25 01:16:32 +0530

    Completed spoiler blocker for Relay for reddit app

--- app/src/main/java/spoiler/blocker/OverlayView.kt
+++ app/src/main/java/spoiler/blocker/OverlayView.kt
@@ -2,6 +2,7 @@
 package spoiler.blocker
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
@@ -11,21 +12,45 @@ import android.view.View
 import androidx.core.graphics.toRectF
 import spoiler.blocker.util.makeGone
 import spoiler.blocker.util.makeVisible
+import java.util.*
 
 /**
  * Created by Muthuraj on 04/03/22.
  */
 class OverlayView(context: Context) : View(context) {
 
+    private data class TextData(val text: String, val x: Float, val y: Float)
 
-    fun addRect(rect: Rect) {
+    //To draw the text that caused the element to be blocked in that element itself as overlay.
+    private val textDataList = mutableListOf<TextData>()
+
+    fun addRect(rect: Rect, text: String?) {
         makeVisible()
         path.addRect(rect.toRectF(), Path.Direction.CW)
+        if (text != null) {
+            val finalText = text.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(Locale.ROOT)
+                else it.toString()
+            }
+            val textWidth = textPaint.measureText(finalText)
+            textDataList.add(
+                TextData(
+                    text.replaceFirstChar {
+                        if (it.isLowerCase()) it.titlecase(
+                            Locale.ROOT
+                        ) else it.toString()
+                    },
+                    x = rect.centerX() - (textWidth / 2f),
+                    y = rect.centerY().toFloat()
+                )
+            )
+        }
         invalidate()
     }
 
     fun clearAllRect() {
         path.reset()
+        textDataList.clear()
         makeGone()
     }
 
@@ -37,11 +62,22 @@ class OverlayView(context: Context) : View(context) {
         }
     }
 
+    private val textPaint by lazy {
+        Paint().apply {
+            isAntiAlias = true
+            color = Color.WHITE
+            textSize = Resources.getSystem().displayMetrics.scaledDensity * 12
+        }
+    }
+
     private val path = Path()
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         canvas.drawPath(path, paint)
+        textDataList.forEach {
+            canvas.drawText(it.text, it.x, it.y, textPaint)
+        }
     }
 }
--- app/src/main/java/spoiler/blocker/SpoilerBlockerService.kt
+++ app/src/main/java/spoiler/blocker/SpoilerBlockerService.kt
@@ -2,21 +2,17 @@
 package spoiler.blocker
 
 import android.accessibilityservice.AccessibilityService
-import android.graphics.Color
 import android.graphics.PixelFormat
 import android.graphics.Rect
-import android.graphics.drawable.ColorDrawable
-import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.content.getSystemService
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import spoiler.blocker.util.children
 import spoiler.blocker.util.log
 import spoiler.blocker.util.logE
+import spoiler.blocker.util.logI
 import spoiler.blocker.util.makeGone
 
 
@@ -24,6 +20,16 @@ import spoiler.blocker.util.makeGone
  * Created by Muthuraj on 03/03/22.
  */
 class SpoilerBlockerService : AccessibilityService() {
+
+    override fun onCreate() {
+        logE { "onCreate() called" }
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        logE { "onDestroy() called" }
+        super.onDestroy()
+    }
 
     private val windowManager by lazy {
         getSystemService<WindowManager>()!!
@@ -42,93 +48,88 @@ class SpoilerBlockerService : AccessibilityService() {
             layoutParams = windowManagerLayoutParams
         }
         view.makeGone()
-//        val view = FrameLayout(this).apply {
-//            background = ColorDrawable(Color.RED)
-//        }
         windowManager.addView(view, windowManagerLayoutParams)
         view
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        log { "onAccessibilityEvent() called with: event = [$event]" }
+        logI { "onAccessibilityEvent() called with: event = [$event]" }
+        if (event.className == "android.widget.ProgressBar") {
+            //When video/image loads, progress bar will be updated very frequently and we don't want
+            //to update overlay view that frequently. Just ignore progress bar events.
+            return
+        }
 
-//        logE { "overlay views size before removing: ${overlayViews.size}" }
-//        if (overlayView.isAttachedToWindow) {
-//            windowManager.removeView(overlayView)
-//        }
+        if (event.packageName == "com.android.systemui") {
+            //Whenever Status bar updates and heads up notifications are updated, accessibility event
+            //from this package will be delivered. We don't want to act on these as this is not from
+            //full blown app.
+            return
+        }
+
+        //TODO split screen app?
+
         overlayView.clearAllRect()
-//        overlayViews.forEach {
-//            overlayView.removeView(it)
-//        }
-//        overlayViews.clear()
 
         if ("reddit.news" in event.packageName) {
             //Reddit app.
             checkForReddit(rootInActiveWindow)
             return
         }
-//        mDebugDepth = 0
-//        val list = mutableListOf<String>()
-//        printAllViews(rootInActiveWindow, list)
-//        if (list.any { it.contains("Console") }) {
-//            log { "Reddit view detected." }
-//        }
     }
-
-//    private val overlayViews = mutableListOf<View>()
 
     private fun checkForReddit(nodeInfo: AccessibilityNodeInfo?) {
         nodeInfo ?: return
 
-        val dy = windowManager.currentWindowMetrics.windowInsets.getInsets(WindowInsets.Type.statusBars()).top
+        val dy =
+            windowManager.currentWindowMetrics.windowInsets.getInsets(WindowInsets.Type.statusBars()).top
         val rect = Rect()
+        var blockedText: String? = null
         nodeInfo.children.forEachIndexed { index, childNode ->
-            if (childNode.viewIdResourceName == "reddit.news:id/title") {
-                logE { "Found title with text: \"${childNode.text}\" at index: $index" }
+            log { "child id: ${childNode.viewIdResourceName}, text: ${childNode.text}" }
+            when (childNode.viewIdResourceName) {
+                "reddit.news:id/title" -> {
+                    if (blockedText == null) {
+                        blockedText = childNode.getBlockedTextIfFound()
+                    }
+                    if (blockedText != null) {
+                        logE { "Blocking title with text: \"${childNode.text}\" at index: $index" }
 
-                childNode.getBoundsInScreen(rect)
-                rect.offset(0, -dy)
-                overlayView.addRect(rect)
-
-                nodeInfo.children.forEachIndexed { childIndex, sibling ->
-                    if (childIndex > index && sibling.viewIdResourceName == "reddit.news:id/imagePreview") {
-                        sibling.getBoundsInScreen(rect)
-                        logE { "Found imagePreview with text: \"${sibling.text}\" at index: $index for text: \"${childNode.text}\", rect: $rect" }
+                        childNode.getBoundsInScreen(rect)
                         rect.offset(0, -dy)
-                        overlayView.addRect(rect)
-//                        val params = FrameLayout.LayoutParams(rect.width(), rect.height())
-//                        val view = View(this).apply {
-//                            background = ColorDrawable(Color.RED)
-//                            layoutParams = params
-//                        }
-//                        view.x = rect.left.toFloat()
-//                        view.y = rect.top.toFloat()
-//                        overlayView.addView(view, rect.width(), rect.height())
-//                        if (overlayView.isAttachedToWindow.not()) {
-//                            windowManager.addView(overlayView, windowManagerLayoutParams)
-//                        }
-                        logE { "adding overlay view width: ${rect.width()}, height: ${rect.height()}" }
-//                        overlayViews += view
+                        overlayView.addRect(rect, blockedText)
+                    } else {
+                        log { "Found title with text: \"${childNode.text}\" at index: $index" }
+                    }
 
-                        /*val windowManagerLayoutParams = WindowManager.LayoutParams().apply {
-                            type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-                            format = PixelFormat.TRANSLUCENT
-                            flags = flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                            width = rect.width()
-                            height = rect.height()
-                            x = rect.left
-                            y = rect.top
+                    nodeInfo.children.forEachIndexed { childIndex, sibling ->
+                        if (blockedText != null) {
+                            if (childIndex > index && sibling.viewIdResourceName == "reddit.news:id/imagePreview") {
+                                sibling.getBoundsInScreen(rect)
+                                logE { "Found imagePreview with text: \"${sibling.text}\" at index: $index for text: \"${childNode.text}\", rect: $rect" }
+                                rect.offset(0, -dy)
+                                overlayView.addRect(rect, blockedText)
+                                logE { "adding overlay view width: ${rect.width()}, height: ${rect.height()}" }
+                            }
                         }
-                        val view = View(this).apply {
-                            background = ColorDrawable(Color.RED)
-                        }
-                        windowManager.addView(view, windowManagerLayoutParams)
-                        logE { "adding overlay view count: ${overlayViews.size + 1}" }
-                        overlayViews += view*/
                     }
                 }
-            } else {
-                checkForReddit(childNode)
+                "reddit.news:id/selftext", "reddit.news:id/imagePreview", "reddit.news:id/video", "reddit.news:id/linkFlair" -> {
+                    if (blockedText == null) {
+                        blockedText = childNode.getBlockedTextIfFound()
+                    }
+                    if (blockedText != null) {
+                        logE { "Blocking title with self text: \"${childNode.text}\" at index: $index" }
+                        childNode.getBoundsInScreen(rect)
+                        rect.offset(0, -dy)
+                        overlayView.addRect(rect, blockedText)
+                    } else {
+                        log { "Found title with self text: \"${childNode.text}\" at index: $index" }
+                    }
+                }
+                else -> {
+                    checkForReddit(childNode)
+                }
             }
         }
     }
@@ -154,5 +155,30 @@ class SpoilerBlockerService : AccessibilityService() {
 
     override fun onInterrupt() {
         log { "onInterrupt() called" }
+    }
+
+    companion object {
+        private val blockList =
+            listOf(
+                "strange",
+                "doctor strange",
+                "dr strange",
+                "madness",
+                "multiverse",
+                "moon knight",
+                "Love and Thunder"
+            )
+
+        private fun AccessibilityNodeInfo.getBlockedTextIfFound(): String? {
+            val text = text ?: return null
+            val result = blockList.find { text.contains(it, ignoreCase = true) }
+            if (result != null) {
+                logE { "shouldBlock() called true $text" }
+            } else {
+                log { "shouldBlock() called false" }
+            }
+            return result
+//            return blockList.any { it in text }
+        }
     }
 }
